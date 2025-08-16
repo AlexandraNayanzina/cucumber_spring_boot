8a0625e83dd33b082bc25f62972ef03a74da9d84
AlexandraN <alexandranayanzina@gmail.com>
2025-08-16 15:09:35 -0400

    commit

--- src/test/java/com/example/springbootforautotesting2025/runner/CucumberTestRunner.java
+++ src/test/java/com/example/springbootforautotesting2025/runner/CucumberTestRunner.java
@@ -4,10 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = {"com.example.springbootforautotesting2025.steps"},
-        plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json"},
-        monochrome = true
+        features = "classpath:features",
+        glue = {"com.example.springbootforautotesting2025"},
+        plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json", "summary"},
+        monochrome = true,
+        objectFactory = io.cucumber.spring.CucumberSpringObjectFactory.class
 )
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
 
